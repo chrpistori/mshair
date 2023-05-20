187790c5492e7c0b5cdb5b024e749fc31022cb90
chrpistori <chr.pistori@gmail.com>
2023-05-19 22:38:45 -0300

    Correcting commits.

--- src_/main/kotlin/com/dev/chris/salaomshair/SalaomshairApplication.kt
+++ src_/main/kotlin/com/dev/chris/salaomshair/SalaomshairApplication.kt
@@ -1,11 +1,11 @@
-package com.dev.chris.salaomshair
+package com.dev.chris.mshair
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SalaomshairApplication
+class mshairApplication
 
 fun main(args: Array<String>) {
-	runApplication<SalaomshairApplication>(*args)
+	runApplication<mshairApplication>(*args)
 }
--- src_/test/kotlin/com/dev/chris/salaomshair/SalaomshairApplicationTests.kt
+++ src_/test/kotlin/com/dev/chris/salaomshair/SalaomshairApplicationTests.kt
@@ -1,10 +1,10 @@
-package com.dev.chris.salaomshair
+package com.dev.chris.mshair
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class SalaomshairApplicationTests {
+class mshairApplicationTests {
 
 	@Test
 	fun contextLoads() {
