bc4467fd827ed747a27f5fed720f3502b3550ac4
chrpistori <chr.pistori@gmail.com>
2023-05-19 22:17:51 -0300

    Creating Kotlin SpringBoot project.

---
+++ src/main/kotlin/com/dev/chris/salaomshair/SalaomshairApplication.kt
@@ -0,0 +1,11 @@
+package com.dev.chris.salaomshair
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SalaomshairApplication
+
+fun main(args: Array<String>) {
+	runApplication<SalaomshairApplication>(*args)
+}
---
+++ src/test/kotlin/com/dev/chris/salaomshair/SalaomshairApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dev.chris.salaomshair
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SalaomshairApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
